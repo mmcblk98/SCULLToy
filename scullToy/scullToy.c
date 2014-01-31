3a6dc53ae77308c491ffe68a8e17e05e4946e23b
joseph98 <joseph98@joseph98-K43E.(none)>
2014-01-31 14:30:59 +0800

    [Construct] Setup an essential framework

--- scullToy/scullToy.c
+++ scullToy/scullToy.c
@@ -1,15 +1,39 @@
-#include <linux/init.h>
+// ======================= Basic kernel and module header =======================
+#include <linux/kernel.h>
 #include <linux/module.h>
+// ======================= Essential Device Driver Include  =====================
+#include <linux/fs.h>
+#include <linux/init.h>
 
 MODULE_LICENSE("Dule BSD/GPL");
+
+/* ============================================================================
+ * Char Device Operation Definition & Implementation
+ *   |- Only four required operations are implemented here:
+ *     |- open, release, read, write
+ * ============================================================================*/
+
+static int scullToy_open(struct inode *inode, struct file *filp)
+{
+}
+static int scullToy_release(struct inode *inode, struct file *filp)
+{
+}
+static ssize_t scullToy_read(struct file *filp, char __user *buff, size_t count, loff_t *f_ops)
+{
+}
+static ssize_t scullToy_write(struct file *flip, const __user *buff, size_t count, loff_t *f_ops)
+{
+}
+
 
 /* ============================================================================
  * Module init and exit
  * ============================================================================*/
 static int __init scullToy_init(void)
 {
-    printk(KERN_ALERT "==> SCULLToy: Init...\n");
-    return 0;
+	printk(KERN_ALERT "==> SCULLToy: Init...\n");
+	return 0;
 }
 
 
