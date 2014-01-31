2fb701c546954f7cdf2b25b726ab8cdf78f82d2d
joseph98 <joseph98@joseph98-K43E.(none)>
2014-01-31 18:39:48 +0800

    [linux] Add linux source to easily use ctags...

--- scullToy/scullToy.c
+++ scullToy/scullToy.c
@@ -1,11 +1,20 @@
+/* ============================================================================
+ * Breakable Toy of Char Device Driver Exevise
+ * 
+ * Reference to Ch3 in Linux Device Driver 3rd
+ * This device only considers the basic operation of Char Device Driver,
+ * so advanced functions such as ioctl are not shown here.
+ * ============================================================================*/
+
 // ======================= Basic kernel and module header =======================
 #include <linux/kernel.h>
 #include <linux/module.h>
 // ======================= Essential Device Driver Include  =====================
-#include <linux/fs.h>
-#include <linux/init.h>
+#include <linux/fs.h>		
+#include <linux/init.h>		//Module Init API
+#include <linux/cdev.h>		//Char Device API
 
-MODULE_LICENSE("Dule BSD/GPL");
+#define DEVICE_NAME	"scullToy"
 
 /* ============================================================================
  * Char Device Operation Definition & Implementation
@@ -15,17 +24,28 @@ MODULE_LICENSE("Dule BSD/GPL");
 
 static int scullToy_open(struct inode *inode, struct file *filp)
 {
+	return 0;
 }
 static int scullToy_release(struct inode *inode, struct file *filp)
 {
+	return 0;
 }
 static ssize_t scullToy_read(struct file *filp, char __user *buff, size_t count, loff_t *f_ops)
 {
+	return 0;
 }
 static ssize_t scullToy_write(struct file *flip, const __user *buff, size_t count, loff_t *f_ops)
 {
+	return 0;
 }
 
+struct file_operations scullToy_fops = {			// 初始化file_operations結構
+	.owner =	THIS_MODILE,						// 用來連結裝置的functions
+	.open =		scullToy_open,
+	.release =	scullToy_release,
+	.read =		scullToy_read,
+	.write =	scullToy_write,
+};
 
 /* ============================================================================
  * Module init and exit
@@ -33,6 +53,7 @@ static ssize_t scullToy_write(struct file *flip, const __user *buff, size_t coun
 static int __init scullToy_init(void)
 {
 	printk(KERN_ALERT "==> SCULLToy: Init...\n");
+
 	return 0;
 }
 
